5f7b20a65672066e006d82153df811f9f8fe6c34
ViAarisKea <viaa0001@stud.kea.dk>
2024-10-30 09:19:46 +0100

    add user service

--- src/main/java/kea/wishlist/controller/UserController.java
+++ src/main/java/kea/wishlist/controller/UserController.java
@@ -1,14 +1,30 @@
 package kea.wishlist.controller;
 
+import kea.wishlist.model.User;
+import kea.wishlist.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/register")
     public String register(){
         return "registerForm";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+
+        return "redirect:/login";
     }
 
 }
--- src/main/java/kea/wishlist/repo/UserRepository.java
+++ src/main/java/kea/wishlist/repo/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 public class UserRepository {
 
 
+
 }
