ac25eadc9853f660194e8e53dfc45bb443ce435a
ViAarisKea <viaa0001@stud.kea.dk>
2024-10-30 10:05:32 +0100

    add saveuser

--- src/main/java/kea/wishlist/repo/UserRepository.java
+++ src/main/java/kea/wishlist/repo/UserRepository.java
@@ -1,11 +1,22 @@
 package kea.wishlist.repo;
 
 
+import kea.wishlist.util.ConnectionManager;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
 
 @Repository
 public class UserRepository {
+    private ConnectionManager connectionManager;
 
+    public UserRepository(ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
 
+    public User saveUser(User user){
+        Connection connection = connectionManager.get
+    }
 
 }
