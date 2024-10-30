fddaa86871be381b25ad69bb77da4369439e439e
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2024-10-30 08:25:12 +0100

    added start conent to WishModel and added DDL, DML sql files.

--- src/main/java/kea/wishlist/model/WishModel.java
+++ src/main/java/kea/wishlist/model/WishModel.java
@@ -1,4 +1,60 @@
 package kea.wishlist.model;
 
 public class WishModel {
+    private String name;
+    private String description;
+    private String url;
+    private double price;
+    private String imgUrl;
+
+    public WishModel(String name, String description, String url, double price, String imgUrl) {
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public WishModel(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
 }
