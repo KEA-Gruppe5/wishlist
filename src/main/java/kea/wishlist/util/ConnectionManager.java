b6aadbaea2a6336dccaebfa2a3c3f4133edd5985
ViAarisKea <viaa0001@stud.kea.dk>
2024-10-30 09:55:36 +0100

    make ConnectionManager a component, move value injection to constructor, remove method parameters in getConnection

--- src/main/java/kea/wishlist/util/ConnectionManager.java
+++ src/main/java/kea/wishlist/util/ConnectionManager.java
@@ -1,29 +1,31 @@
 package kea.wishlist.util;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Component
 public class ConnectionManager {
+
     private static Connection connection;
 
-    @Value("${spring.datasource.url}")
-    private String URL;
-    @Value("${spring.datasource.username}")
-    private String USERNAME;
-    @Value("${spring.datasource.password}")
-    private String PASSWORD;
+    private final String URL;
+    private final String USERNAME;
+    private final String PASSWORD;
 
 
-    public ConnectionManager(String URL, String USERNAME, String PASSWORD) {
+    public ConnectionManager(@Value("${spring.datasource.url}") String URL,
+                             @Value("${spring.datasource.username}")String USERNAME,
+                             @Value("${spring.datasource.password}")String PASSWORD) {
         this.URL = URL;
         this.USERNAME = USERNAME;
         this.PASSWORD = PASSWORD;
     }
 
-    private static Connection getConnection(String URL, String USERNAME, String PASSWORD){
+    private Connection getConnection(){
         if (connection != null){
             return connection;
         }
