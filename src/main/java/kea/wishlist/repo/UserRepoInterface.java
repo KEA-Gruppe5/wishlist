55c09f400a8d840dcb6f530f6d1ac32f0ab55c99
ViAarisKea <viaa0001@stud.kea.dk>
2024-10-31 14:31:36 +0100

    refactor interface name and add implementing

--- src/main/java/kea/wishlist/repo/UserRepoInterface.java
+++ src/main/java/kea/wishlist/repo/UserRepoInterface.java
@@ -4,11 +4,9 @@ import kea.wishlist.model.User;
 
 import java.sql.SQLException;
 
-public interface userRepoInterface {
+public interface UserRepoInterface {
     User addUser(User user) throws SQLException;
-    User updateUser(User user int id);
+    User updateUser(User user, int id);
     boolean deletedUser(int id) throws SQLException;
     User findUserWithId (int id);
-
-
 }
