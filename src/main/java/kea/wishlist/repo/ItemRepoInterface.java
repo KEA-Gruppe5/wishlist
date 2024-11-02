ed1e11d7387320d5629f3ed41a5c25b3751a1d00
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2024-11-02 13:01:19 +0100

    added delete method

---
+++ src/main/java/kea/wishlist/repo/ItemRepoInterface.java
@@ -0,0 +1,13 @@
+package kea.wishlist.repo;
+
+import kea.wishlist.model.ItemModel;
+import kea.wishlist.model.User;
+
+import java.sql.SQLException;
+
+public interface ItemRepoInterface {
+    ItemModel addItem(ItemModel item) throws SQLException;
+    ItemModel updateItem(ItemModel item, int id);
+    boolean deleteItem(int id) throws SQLException;
+    User findItemById (int id);
+}
