ac00e13921baf1882cd69a9bbe2f66af6074e16d
ViAarisKea <viaa0001@stud.kea.dk>
2024-11-05 23:14:15 +0100

    modify sendEmail test

--- src/test/java/kea/wishlist/service/EmailServiceTest.java
+++ src/test/java/kea/wishlist/service/EmailServiceTest.java
@@ -1,20 +1,33 @@
 package kea.wishlist.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.times;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class EmailServiceTest {
 
-   @Autowired
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Autowired
     private EmailService emailService;
 
     @Test
-    void testSendEmail(){
-        emailService.sendEmail("viktoria.aaris@gmail.com");
+    void testSendEmail() {
+        emailService.sendEmail("test@example");
+        Mockito.verify(javaMailSender, times(1)).send(Mockito.any(SimpleMailMessage.class));
     }
 
 }
