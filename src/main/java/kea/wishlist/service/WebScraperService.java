4fe466bae2ea693ee3ab93bd516464090e2c1099
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2024-11-13 10:38:37 +0100

    added useragent to mimic browser

--- src/main/java/kea/wishlist/service/WebScraperService.java
+++ src/main/java/kea/wishlist/service/WebScraperService.java
@@ -15,7 +15,10 @@ public class WebScraperService {
         Item item = new Item();
 
         try {
-            Document document = Jsoup.connect(url).get();
+            Document document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+                    .header("Accept-Language", "en-US,en;q=0.9")
+                    .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+                    .get();
 
             String title = document.select("h1").text();
             item.setName(title);
